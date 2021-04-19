8b4aa455329deeca821dcce89daeda99811a15c5
NamNV5 <nguyennam16051997@gmail.com>
2021-04-19 18:01:55 +0700

    commit edit

--- src/main/java/com/example/TranslationServlet/TranslationServlet.java
+++ src/main/java/com/example/TranslationServlet/TranslationServlet.java
@@ -1,6 +1,35 @@
 package com.example.TranslationServlet;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+@WebServlet(name = "TranslationServlet" , urlPatterns = "/translate")
 public class TranslationServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển sách");
+        dictionary.put("computer", "Máy tính");
+
+        String search = request.getParameter("txtSearch");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        String result = dictionary.get(search);
+        if (result !=null){
+            writer.println("word: " + search);
+            writer.println("Result: " + result);
+        }else {
+            writer.println("not found");
+        }
+        writer.println("</html>");
+    }
 }
